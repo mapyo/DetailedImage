d605c4bcdf543117765299433d276d7769cab8fd
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2016-02-16 08:18:21 +0900

    initial commit

---
+++ sample/src/main/java/com/mapyo/sample/detailedimage/MainActivity.java
@@ -0,0 +1,30 @@
+package com.mapyo.sample.detailedimage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.mapyo.detailedimage.DetailedImageActivity;
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView mainImage = (ImageView) findViewById(R.id.main_image);
+
+        final String imageUrl = "https://raw.githubusercontent.com/mapyo/sample/master/image/nyannyan.png";
+        Picasso.with(this).load(imageUrl).into(mainImage);
+
+        mainImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DetailedImageActivity.launchActivity(MainActivity.this, imageUrl);
+            }
+        });
+    }
+}
